55a7c9c1015faacd6439d5a8a575ff3b528506da
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-07-12 17:17:03 +0300

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,40 @@
+// check out the workshop tab to get started
+// welcome to blot!
+
+// check out this guide to learn how to program in blot
+// https://blot.hackclub.com/editor?guide=start
+
+const width = 125;
+const height = 125;
+
+const rectW = 3
+const rectH = 121
+const hexHorL = 30
+const hexDiagL = 60
+
+const shape = (n) => {
+  const t = new bt.Turtle()
+  for (let i = 0; i < n; i++) {
+    t.forward(1).right(360/n)
+  }
+  return t.lines();
+}
+
+const shaft = bt.scale(shape(3), [2, 150])
+const vanes = bt.scale(shape(11), [8, 30])
+bt.translate(vanes, [0, bt.bounds(shaft).cb[1] - bt.bounds(vanes).cb[1]])
+
+const feather = [...shaft, ...vanes]
+const center = bt.translate(feather, [width/2, height/2], bt.bounds(feather).cc)
+
+bt.resample(feather, 4)
+
+bt.iteratePoints(feather, (pt, t) => {
+  const [x, y] = pt
+  return [x - 0.002*(width/2-y)*(width/2-y), y]
+})
+
+bt.rotate(feather, 135)
+
+
+drawLines(feather)
