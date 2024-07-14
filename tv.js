361d6558f4b3292f8c0f2a7fad01f093a92761a7
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-07-14 22:28:55 +0300

    Continued TV

--- tv.js
+++ tv.js
@@ -11,6 +11,7 @@ const tvHeight = 180
 const tvBezel = 7
 const tvCornerRad = 2
 const tvScreenDeg = -20
+const tvPanelW = 40
 
 setDocDimensions(width, height);
 
@@ -28,6 +29,20 @@ const getChord = (radius, arcDeg) => {
   return chord
 }
 
+const createGrill = (length, distanceBetween, width) => {
+  const lineCount = width / distanceBetween
+  if (typeof lineCount !== 'integer') throw new Error('createGrill(): width / distanceBetween must be an integer!')
+
+  for (let i = 0; i++; i >= lineCount/2) {
+    t.forward(length)
+    t.left(90).up()
+    t.forward(distanceBetween).left(90).down()
+    t.forward(length)
+    t.up().right(90).forward(distanceBetween)
+    t.right(90).down()
+  }
+}
+
 const t = new bt.Turtle();
 
 t.left(10)
@@ -39,14 +54,54 @@ t.arc(tvScreenDeg, tvWidth)
 t.arc(-70, tvCornerRad)
 t.arc(tvScreenDeg, tvHeight)
 t.arc(-70, tvCornerRad)
+
 t.left(170).up()
 t.forward(tvCornerRad)
-t.right(90).down()
-t.forward(tvBezel)
 t.right(90)
-t.forward(getChord(tvWidth, tvScreenDeg)).forward(tvCornerRad*2 + tvBezel)
+t.forward(tvBezel)
+t.right(90).down()
+
+t.forward(getChord(tvWidth, tvScreenDeg)).forward(tvCornerRad + tvBezel)
 t.arc(-90, tvCornerRad)
-t.forward(1)
+t.forward(getChord(tvHeight, tvScreenDeg)).forward(tvBezel * 2)
+t.arc(-90, tvCornerRad)
+t.forward(getChord(tvWidth, tvScreenDeg)).forward(tvBezel * 2)
+t.arc(-90, tvCornerRad)
+t.forward(getChord(tvHeight, tvScreenDeg)).forward(tvBezel * 2)
+t.arc(-90, tvCornerRad)
+t.forward(tvBezel).up()
+
+t.forward(getChord(tvWidth, tvScreenDeg) + tvBezel).down()
+
+t.forward(tvPanelW)
+t.arc(-90, tvCornerRad)
+t.forward(getChord(tvHeight, tvScreenDeg) + 2 * tvBezel)
+t.arc(-90, tvCornerRad)
+t.forward(tvPanelW).up()
+
+t.left(180).forward(tvCornerRad)
+t.forward(tvPanelW / 8)
+t.left(90).forward((getChord(tvHeight, tvScreenDeg) +  tvBezel + tvCornerRad))
+t.down()
+
+t.arc(-90, tvCornerRad)
+t.forward(tvPanelW * (6/8) - 2 * tvCornerRad)
+t.arc(-90, tvCornerRad)
+t.forward(getChord(tvHeight, tvScreenDeg) / 2)
+t.arc(-90, tvCornerRad)
+t.forward(tvPanelW * (6/8) - 2 * tvCornerRad)
+t.arc(-90, tvCornerRad)
+t.forward(getChord(tvHeight, tvScreenDeg) / 2)
+
+t.up()
+t.arc(-90, tvCornerRad)
+t.forward((tvPanelW * (6/8) - 2 * tvCornerRad) / 2)
+t.right(90).forward(getChord(tvHeight, tvScreenDeg) / 2)
+t.down()
+
+t.forward(100)
+
+
 
 
 
@@ -60,3 +115,4 @@ const centered = bt.translate(t.lines(), [height / 2, width / 2], cc);
 
 // draw it
 drawLines(centered);
+
