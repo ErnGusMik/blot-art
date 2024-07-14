56fbf583c5e7a47832cec7fa47345ea0917593a2
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-07-14 20:31:56 +0300

    First time using trigonometry IRL :)

--- tv.js
+++ tv.js
@@ -6,34 +6,48 @@
 const width = 180;
 const height = 180;
 
-const tvWidth = 80
-const tvHeight = 60
+const tvWidth = 230
+const tvHeight = 180
 const tvBezel = 7
+const tvCornerRad = 2
+const tvScreenDeg = -20
 
 setDocDimensions(width, height);
 
+// Helper functions
+const toRadians = (degrees) => {
+  return degrees * (Math.PI / 180)
+}
+
+// Get chord of circle from arc degrees & radius
+const getChord = (radius, arcDeg) => {
+  // Get radians from arc width in degrees & divide in half
+  const radians = toRadians(Math.abs(arcDeg) / 2)
+  // Formula: 2 * radius * sin(arc degrees / 2)
+  const chord = 2 * radius * Math.sin(radians)
+  return chord
+}
+
 const t = new bt.Turtle();
 
-t.forward(tvWidth);
-t.arc(90, 3);
-t.forward(tvHeight);
-t.arc(90, 3);
-t.forward(tvWidth);
-t.arc(90, 3);
-t.forward(tvHeight);
-t.arc(90, 3);
-t.up().right(90).forward(tvBezel + 2.5).left(90).down()
-t.forward(tvWidth + 4 * tvBezel)
-t.arc(90, 5);
-t.forward(tvHeight + 2 * tvBezel)
-t.arc(90, 5);
-t.forward(tvWidth + 5 * tvBezel)
-t.arc(90, 5)
-t.forward(tvHeight + 2 * tvBezel)
-t.arc(90, 5)
-t.forward(tvBezel);
-t.up().left(90).forward(tvBezel + 2.5).right(45).down()
-t.forward(tvHeight)
+t.left(10)
+t.arc(-20, tvWidth)
+t.arc(-70, tvCornerRad)
+t.arc(tvScreenDeg, tvHeight)
+t.arc(-70, tvCornerRad)
+t.arc(tvScreenDeg, tvWidth)
+t.arc(-70, tvCornerRad)
+t.arc(tvScreenDeg, tvHeight)
+t.arc(-70, tvCornerRad)
+t.left(170).up()
+t.forward(tvCornerRad)
+t.right(90).down()
+t.forward(tvBezel)
+t.right(90)
+t.forward(getChord(tvWidth, tvScreenDeg)).forward(tvCornerRad*2 + tvBezel)
+t.arc(-90, tvCornerRad)
+t.forward(1)
+
 
 
 
@@ -46,4 +60,3 @@ const centered = bt.translate(t.lines(), [height / 2, width / 2], cc);
 
 // draw it
 drawLines(centered);
-
